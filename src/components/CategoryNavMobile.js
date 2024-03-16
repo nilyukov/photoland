d04934e34a53ae1270d04815b1520fa44898f4ce
Nikita Ilyukov <nilyukov@gmail.com>
2024-03-16 12:45:12 +0300

    feat(CategoryNavMobile): Add links

--- src/components/CategoryNavMobile.js
+++ src/components/CategoryNavMobile.js
@@ -1,7 +1,11 @@
 import PropTypes from 'prop-types';
 import { FiX } from 'react-icons/fi';
+import useFetch from '../hooks/useFetch';
+import { Link } from 'react-router-dom';
 
 const CategoryNavMobile = ({ setCatNavMobile }) => {
+    const { data } = useFetch('/categories');
+
     return (
         <div className="w-full h-full bg-primary p-8">
             <div
@@ -9,6 +13,17 @@ const CategoryNavMobile = ({ setCatNavMobile }) => {
                 className="flex justify-end mb-8 cursor-pointer">
                 <FiX className="text-3xl" />
             </div>
+            <nav className="flex flex-col gap-y-8">
+                {data?.map((category) => (
+                    <Link
+                        key={category.id}
+                        to={`/products/${category.id}`}
+                        className="uppercase font-medium"
+                        onClick={() => setCatNavMobile(false)}>
+                        {category.attributes.title} Cameras
+                    </Link>
+                ))}
+            </nav>
         </div>
     );
 };
